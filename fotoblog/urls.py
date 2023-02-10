c4732552a551bce753c0d3dfc15094a4f9f9886d
giraldiego <giraldiego@gmail.com>
2023-02-10 10:19:31 -0500

    Refactor to use Generic Views for authentication

--- fotoblog/urls.py
+++ fotoblog/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import (
+    LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView)
 from django.urls import path
 import authentication.views
 import blog.views
@@ -9,11 +10,24 @@ urlpatterns = [
     path(
         "",
         LoginView.as_view(
-            template_name="authentication/login.html",
-            redirect_authenticated_user=True
+            template_name="authentication/login.html", redirect_authenticated_user=True
         ),
         name="login",
     ),
-    path("logout/", authentication.views.logout_user, name="logout"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path(
+        "change-password/",
+        PasswordChangeView.as_view(
+            template_name="authentication/password_change_form.html"
+        ),
+        name="password_change",
+    ),
+    path(
+        "change-password-done/",
+        PasswordChangeDoneView.as_view(
+            template_name="authentication/password_change_done.html"
+        ),
+        name="password_change_done",
+    ),
     path("home/", blog.views.home, name="home"),
 ]
