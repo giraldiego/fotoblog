492412db1dafc53aa848f01d51352c101fa7fc0f
giraldiego <giraldiego@gmail.com>
2023-02-10 19:11:32 -0500

    Create an Image Upload Facility

---
+++ blog/forms.py
@@ -0,0 +1,10 @@
+# blog/forms.py
+from django import forms
+
+from . import models
+
+
+class PhotoForm(forms.ModelForm):
+    class Meta:
+        model = models.Photo
+        fields = ["image", "caption"]
--- fotoblog/urls.py
+++ fotoblog/urls.py
@@ -1,3 +1,6 @@
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.contrib import admin
 from django.contrib.auth.views import (
     LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView)
@@ -31,4 +34,9 @@ urlpatterns = [
     ),
     path("signup/", authentication.views.signup_page, name="signup"),
     path("home/", blog.views.home, name="home"),
+    path('photo/upload/', blog.views.photo_upload, name='photo_upload')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
